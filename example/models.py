46d66fc10b946698f27b2d838cd1413ae27cb0b6
Bittersweet Dev <bittersweet-dev@naver.com>
2020-03-20 08:45:47 +0000

    Register at INSTALLED_APPS in settings.py
    And then,
    $ python manage.py makemigrations
    $ python manage.py migrate

--- example/models.py
+++ example/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Example(models.Model):
+	title = models.CharField(max_length=200)
+	content = models.TextField()
+
+	def __str__(self):
+		"""A String representation of the model."""
+		return self.title
